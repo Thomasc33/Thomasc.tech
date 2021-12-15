b7ed09d1f72993994d511985e46fce174bafcd7d
Thomas Carr <35206339+Tcarr9442@users.noreply.github.com>
2021-12-14 20:59:45 -0500

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,10 +9,8 @@ function HomePage(props) {
         <div style={{ position: 'absolute', top: `${headerHeight}px`, width: '100vw', height: '92vh', color: 'white', overflow: 'scroll' }}>
             <div className='HomePage'>
                 <div className='Photo'>
-                    <img style={{ maxWidth: '90%', maxHeight: '75%' }} alt='thomas' src='https://lh3.googleusercontent.com/ZQOdq7hAPjhJJub4W4z9ZV7SfXV3XPmqpWEhmSP8k7CgYm8tsYguknek_cjegAa37TGs5cXcyBRAUrH3u2owkSNai4VVCkxQBDWHCNcHzoiQox3nBoUlkcoIewARdrl20BWRds6qR61Uwl60JIe8spWRb7ODOgvaD74_YcvrsWgyypfUMDZMvSCR5coKQg2xno7EAJtTnRQLzmksDvhN5VVe73eyC9zZguWY5MlFkZQDGLqkbekijbAJHBqJW9ocCLjgLdHMUaIO9dXlNC1-yYpmhzoO4ninJpeDLE2b0ub8f0jSGcCyx78wuY17SwSps7bIKOq7aavOAoPPn1-h67VbvKu9bY6XIb1pRb9opnmkEEWCw0sXnagnCpIRNbD2_2R_AhfYJj0OQ4gGr1nAMSDsuAIBo5Y4s15eGAu1vnZSbSBQOV2X9E5Ai6xJGytUfx7lVMqeg9HTCrsBEfGULY3avXsUv677zUPJ84cIQPZQmIn_CUBYoX3jX45yU-tA3wskFeXusnHJtxfzbl896qjHlv-vhDYvZ-HHyVqabiAyym7MRr754HXk_K4yOj5wQMRm1bfwKeCsmgYonDqsePbp0NbhYxaTMKpUjNB7IUlp5zSH1XNkHLPFgzZRpQjIh5Y-Z9Aisam6CPBXw1KW-SBCLFqBSnyFSQ8nPSMp3PiDpFZnSjRh8vzBe850alx0-RP2Q2NwL1tIutwG65HitBW0=w440-h977-no?authuser=0' />
-                    <div className='break' />
-                    <p>*This photo will be changed once i get professional shots</p>
-                </div>
+                    <h3>Image will be here soon</h3>
+                    </div>
                 <div className='HomeInfo'>
                     <h1>Hi! I'm Thomas</h1>
                     <div className='break' />
